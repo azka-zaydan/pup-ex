ef5e2173026e65bccbfebe655ed3a428ca9f0b2c
azka1415 <rafifzaydan4@gmail.com>
2022-08-19 13:57:24 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { scrapeImages } from './src/scrape.js'
+
+const app = express()
+
+app.get('/', (req, res) => res.status(200).json({ message: 'server is on' }))
+
+app.get('/scrape/:username', async (req, res) => {
+    const username = req.params.username
+    const data = await scrapeImages(username)
+    res.status(200).json({ data })
+    console.log('data sent')
+})
+
+const PORT = process.env.PORT || 4000
+
+
+app.listen(PORT, (err) => {
+    if (err) throw err
+    console.log(`Listening on Port: ${PORT}`)
+})
+
---
+++ src/scrape.js
@@ -0,0 +1,33 @@
+import puppeteer from 'puppeteer'
+
+export const scrapeImages = async (username) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    })
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+
+    await page.goto(`https://www.instagram.com/${username}`, { waitUntil: 'networkidle0' })
+
+    await page.waitForSelector('img', {
+        visible: true
+    })
+
+    await page.screenshot({ path: `${username}'s-page.png` })
+    // console.log(page.url())
+    const data = await page.evaluate(() => {
+        const images = document.querySelectorAll('img')
+        const urls = Array.from(images).map(i => {
+            const obj = { src: i.src, alt: i.alt }
+            return obj
+        })
+        return urls
+    })
+
+
+    await browser.close()
+
+    return data
+
+}
