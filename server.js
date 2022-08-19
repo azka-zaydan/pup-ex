6c8553916e1e80b94c4602892f7b1c59757d4779
azka1415 <rafifzaydan4@gmail.com>
2022-08-19 15:16:57 +0700

    added instructions

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import { scrapeImages } from './src/scrape.js'
 
 const app = express()
 
-app.get('/', (req, res) => res.status(200).json({ message: 'server is on' }))
+app.get('/', (req, res) => res.status(200).json({
+    message: 'server is on',
+    instructions: 'visit /scrape/:username enter the username of the person, make sure they are not private'
+}))
 
 app.get('/scrape/:username', async (req, res) => {
     const username = req.params.username
